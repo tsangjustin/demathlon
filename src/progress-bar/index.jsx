46ec9ce20eb51808c4b0c9ded4df43dd0629a9a9
tsangjustin <justintcoding@gmail.com>
2018-11-04 16:58:56 -0500

    Change style of progress bar

--- src/progress-bar/index.jsx
+++ src/progress-bar/index.jsx
@@ -5,15 +5,19 @@ import "./progress.css";
 export class ProgressBar extends Component {
   render() {
     const {current, total} = this.props;
+    const percent_complete = current/total*100
     return (
-      <div className="Game-Progress progress progress-striped active">
+      <div id="Game-Progress" className="progress progress-striped active ">
         <div
             className="progress-bar"
+            id="Current-Progress"
             role="progressbar"
-            style={{width: `${current/total*100}%`}}
+            style={{width: `${percent_complete}%`}}
             aria-valuenow={current}
             aria-valuemin="0"
-            aria-valuemax={total} />
+            aria-valuemax={total}>
+          {Math.round(percent_complete)}%
+        </div>
       </div>
     );
   }
